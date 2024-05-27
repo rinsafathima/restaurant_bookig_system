aa66050623f82df31fb97a5ea83a558750d64220
Rinsa Fathima <rinsamrf@gmail.com>
2024-05-27 20:07:57 +0530

    first commit

--- src/components/Frontend/banner.tsx
+++ src/components/Frontend/banner.tsx
@@ -7,16 +7,14 @@ export default function Banner() {
       <div className="mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-4">
         <div className="max-w-xl text-center sm:text-left lg:pl-12">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
-            Bienvenido a
+              Welcome to
             <strong className="block font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-lime-600 to-green-500">
-              Bamboo Express
+              Bug Free Spoon
             </strong>
           </h1>
 
           <p className="mt-4 max-w-lg sm:text-xl sm:leading-relaxed">
-            Nuestras recetas únicas y de alta calidad te transportarán a un
-            viaje culinario por China. Reserva tu mesa ahora y disfruta de una
-            experiencia gastronómica inolvidable !
+          Our unique and high-quality recipes will transport you on a culinary journey through China. Reserve your table now and enjoy an unforgettable dining experience!
           </p>
 
           <div className="mt-8 flex flex-wrap gap-4 text-center">
@@ -28,7 +26,7 @@ export default function Banner() {
           </div>
         </div>
         <div className="mt-12 sm:mt-16 lg:mt-0 lg:ml-16 lg:flex-1 lg:pl-8 pointer-events-none">
-          <Image
+          <img
             alt="banner"
             src="/img/banner.png"
             className="hidden lg:block"
